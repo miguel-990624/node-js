0e92472d223bdfd5a804fb26dedde2e575a8bf1f
miguel-990624 <miguelangelmolinagutierrez@gmail.com>
2025-09-19 18:00:29 -0500

    bug fix

--- Week2/Class9/src/controllers/apiController.ts
+++ Week2/Class9/src/controllers/apiController.ts
@@ -3,8 +3,18 @@ import { getRandomTrivia } from '../services/apiService.ts';
 
 export async function getTriviaController(req: Request, res: Response) {
   try {
-    const fact = await getRandomTrivia();
-    res.json({ success: true, fact });
+    const user = (req as any).user;
+    const numberRequested = (req as any).numberRequested;
+
+    // Obtener trivia del número solicitado
+    const fact = await getRandomTrivia(String(numberRequested));
+
+    res.json({
+      success: true,
+      user,
+      number: numberRequested,
+      fact,
+    })
   } catch (error: any) {
     res.status(500).json({ success: false, message: error.message });
   }
